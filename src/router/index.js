f26459ee8f4ae32630ac6b502ba05912dd6b1bf1
Hector Astorga <hectorastorga105@hotmail.com>
2023-05-13 22:28:12 -0600

    asdf

--- src/router/index.js
+++ src/router/index.js
@@ -3,11 +3,11 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 const routes = [
   {
-    path: '/vue-twotter/',
+    path: '/',
     component: () => import('@/layouts/default/Default.vue'),
     children: [
       {
-        path: '/vue-twotter/',
+        path: '',
         name: 'Home',
         // route level code-splitting
         // this generates a separate chunk (about.[hash].js) for this route
